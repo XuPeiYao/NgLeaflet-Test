d322d3dc7943cac19324791bc82e7fd94ed4f81d
XuPeiYao <st89382000@gmail.com>
2020-03-17 23:20:46 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng, circle, polygon, marker, Map } from 'leaflet';
+import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LeafletTest';
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ],
+    zoom: 5,
+    center: latLng(22.7104955, 120.3078807)
+  };
+
+  layers = [
+    circle([22.7104955, 120.3078807], { radius: 5000 }),
+    marker([22.7104955, 120.3078807])
+  ];
+
+  constructor() {}
+
+  onMapReady(map: Map) {
+    // add a scale at at your map.
+    const scale = L.control.scale().addTo(map);
+  }
 }
